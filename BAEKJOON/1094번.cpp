617cbd66fe36aca4a710dee7a1caadda576ef7cd
KwonSunwon <KwonSunwon@users.noreply.github.com>
2019-11-17 21:32:34 +0900

    Create 1094번.cpp

---
+++ BAEKJOON/1094번.cpp
@@ -0,0 +1,30 @@
+//1094¹ø
+//https://www.acmicpc.net/problem/1094
+
+#include <iostream>
+
+int main()
+{
+	int x, main_stick { 64 }, side_stick {}, sum {}, keep {}, bangal {1};
+
+	std::cin >> x;
+
+	while( true )
+	{
+		side_stick = main_stick / 2;
+		main_stick /= 2;
+
+		if( side_stick + keep >= x )
+			continue;
+		else if( side_stick + main_stick + keep > x )
+		{
+			keep += side_stick;
+			++bangal;
+		}
+		else
+		{
+			std::cout << bangal;
+			break;
+		}
+	}
+}
