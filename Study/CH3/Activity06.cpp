ce8e3ba27f9e05f3782416653158e9d99c7998f1
KwonSunwon <55453462+KwonSunwon@users.noreply.github.com>
2021-06-30 22:43:58 +0900

    Update Activity06.cpp
    내가 std::unordered_map, std::pair를 정확히 이해하지 못해서 second를 붙히는 이유도 이해하지 못한거 같음.
    51번 째 줄 이후를 컴파일 해보고 나서 어느정도 이해가됨

--- Study/CH3/Activity06.cpp
+++ Study/CH3/Activity06.cpp
@@ -16,7 +16,7 @@ public:
 };
 */
 
-void find(const std::unordered_map<std::string,std::string>& container, const std::string& element )
+void findURL(const std::unordered_map<std::string,std::string>& container, const std::string& element )
 {
     auto it = container.find(element);
     if(it == container.end())
@@ -31,21 +31,29 @@ int main()
 
     url.insert(std::pair<std::string, std::string>("navar", "www.navar.com"));
 
-    find( url ,"navar");
+    findURL( url ,"navar");
 
     url.insert(std::pair<std::string,std::string>("google","www.google.co.kr"));
 
-    find( url, "google");
+    findURL( url, "google");
 
-    find( url, "yahoo");
+    findURL( url, "yahoo");
 
-    bool ok = url.insert(std::pair<std::string,std::string>("google", "www.google.com")).second;
-    std::cout << (ok ? "susses" : "failed") << "\n";
-
-    find( url, "google");
+    bool ok = url.insert(std::pair<std::string,std::string>("google", "www.google.com")).second;        //second 는 std::unordered_map::insert의 두번째 return 값인 bool을 돌려주는 역할을 함
+    std::cout << (ok ? "susses" : "failed") << "\n";                                                    //std::unordered::insert 는 std::pair<iterator, bool>을 반환하는데
+                                                                                                        //여기서 두번째 인자, 즉 bool을 인자로 하는 요소가 .second임
+    findURL( url, "google");
 
     url.erase("google");
     url.insert(std::pair<std::string,std::string>("google", "www.google.com"));
-    find(url, "google");
-}
+    findURL(url, "google");
 
+    std::pair<std::unordered_map<std::string, std::string>::iterator ,bool> ok2  = url.insert(std::pair<std::string,std::string>("google.uk", "www.google.co.uk"));
+
+    if (ok2.second)
+    {
+        std::cout << "성공";
+    }
+    
+    
+}
