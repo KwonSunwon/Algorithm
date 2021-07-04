1819d157f36c72f6bca2e47f00ebcd2c83a35c88
KwonSunwon <55453462+KwonSunwon@users.noreply.github.com>
2021-07-04 16:44:46 +0900

    공부중

--- unsolved/1158번.cpp
+++ unsolved/1158번.cpp
@@ -4,20 +4,88 @@
 #include <iostream>
 #include <list>
 
-int main(){
-    int n,k;
+int main()
+{
+    int N, K;
 
-    std::cin >> n;
-    std::cin >> k;
+    std::cin >> N;
+    std::cin >> K;
 
-    std::list<int> yosef;
+    std::list<int> squence;
+    
+    for(int _i = 0 ; _i < N ; ++_i)
+        squence.push_back(_i+1);
 
-    for(int i = 0 ; i < n ; ++i){
-        yosef.push_back(i+1);
+    auto it = squence.begin();
+
+    it++;
+    it++;
+    std::cout << *it << ", ";
+    it = squence.erase(it);
+    it--;
+
+    for(int _i = 0 ; _i < K ; ++_i)
+        {
+            if(it == squence.end())
+                it = squence.begin();
+            ++it;
+        }
+    std::cout << *it << ", ";
+    it = squence.erase(it);
+    it--;
+
+    for(int _i = 0 ; _i < K ; ++_i)
+        {
+            if(it == squence.end())
+                it = squence.begin();
+            ++it;
+        }
+    std::cout << *it << ", ";
+    it = squence.erase(it);
+    it--;
+    
+    for(int _i = 0 ; _i < K ; ++_i)
+        {
+            if(it == squence.end())
+                it = squence.begin();
+            ++it;
+        }
+    std::cout << *it << ", ";
+    it = squence.erase(it);
+    it--;
+    std::cout << "\n (7제거 후 " << *it << ") ";
+
+    for(int _i = 0 ; _i < K ; ++_i)
+        {
+            if(it == squence.end())
+                it = squence.begin();
+            ++it;
+        }
+    std::cout << *it << ", ";
+    it = squence.erase(it);
+    it--;
+
+    std::cout << "\n" << *it;
+
+    ++it;
+    if(it == squence.end())
+        std::cout << "end()\n";
+    std::cout << *it;
+
+/*
+    std::cout << "<";
+    while(!squence.empty())
+    {
+        for(int _i = 0 ; _i < K ; ++_i)
+        {
+            if(it == squence.end())
+                it = squence.begin();
+            ++it;
+        }
+        std::cout << *it << ", ";
+        it = squence.erase(it);
+        it--;
     }
-
-    std::cout << yosef.size();
-
-    for(auto i : yosef)
-        std::cout << i << std::endl;
+    std::cout << ">";
+*/
 }
