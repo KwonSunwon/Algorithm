975b5ac3c27fd45eeb1f436f3f520e0225a57394
KwonSunwon <55453462+KwonSunwon@users.noreply.github.com>
2021-07-04 14:57:32 +0900

    Update 15740번.cpp

--- BAEKJOOON/15740번.cpp
+++ BAEKJOOON/15740번.cpp
@@ -3,64 +3,64 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-using namespace std;
-
-class User{
+class _member
+{
 private:
-    string name;
+    std::string name;
     int age;
-    int sn{};
+    static int count;
+
 public:
-    User(string name, int age, int sn) : name(name), age(age), sn(sn) {  }
-    User(){
-        name = " ";
-        age = 0;
-        sn = 0;
+    _member (int age, std::string name)
+    {
+        this->age = age;
+        this->name = name;
+        this->count = _member::count;
+        _member::count += 1;
     }
 
-    string getName(){
-        return name;
+    static bool compare(const _member& a, const _member& b)
+    {
+        return a.age < b.age;
     }
-
-    int getAge(){
-        return age;
-    }
-
-    int getSn(){
-        return sn;
-    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const _member& p);
 };
 
-int main(){
+int _member::count = 0; 
+
+std::ostream& operator<<(std::ostream& os, const _member& p)
+{
+    os << p.age << " " << p.name;
+    return os;
+}
+
+int main()
+{
     int n;
-    scanf("%d", &n);
 
-    int age;
-    string name;
-    User users[n];
-    
-    for(int i = 0 ; i < n ; ++i){
-        cin >> age >> name;
-        users[i] = User(name, age, i);
+    std::cin >> n;
+
+    std::vector<_member> members;
+
+    int tempAge;
+    std::string tempName;
+
+    for(int _i = 0 ; _i < n ; ++_i)
+    {
+        std::cin >> tempAge;
+        std::cin >> tempName;
+        _member temp = {tempAge, tempName};
+        members.push_back(temp);
     }
 
-    for( int i = 0 ; i < n ; ++i )
-        for(int j = 0 ; j < n ; ++j){
-            if(i != j)
-                if( users[i].getAge() < users[j].getAge()){
-                    User temp = users[i];
-                    users[i] = users[j];
-                    users[j] = temp;
-                }
-                else if(users[i].getAge() == users[j].getAge())
-                    if(users[i].getSn() < users[j].getSn()){
-                        User temp = users[i];
-                        users[i] = users[j];
-                        users[j] = temp;
-                    }
-        }
-    for(int i = 0 ; i < n ; ++i){
-        printf("%d %p", users[i].getAge(), users[i].getName());
-    }
+    stable_sort(members.begin(), members.end(), _member::compare);  //앞뒤 원소의 값이 같을 경우 정렬을 진행하지않음
+                                                                    //즉, 초기 순서 그대로 
+    for(auto i : members)
+        std::cout << i << "\n";
+
+    return 0;
 }
