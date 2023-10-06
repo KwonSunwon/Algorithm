348d9ec9f78d0bef232a8c2f6a96934834c6c0e2
KwonSunwon <kso00011147@gmail.com>
2023-10-06 12:54:55 +0900

    10926번

---
+++ BAEKJOON/10926번.cpp
@@ -0,0 +1,17 @@
+/*
+2023.10.06
+10926번 - ??!
+https://www.acmicpc.net/problem/10926
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s << "??!\n";
+}
