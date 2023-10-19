7c92feaadead73282ba182bebcead5f0671f8681
KwonSunwon <kso00011147@gmail.com>
2023-10-19 22:22:29 +0900

    11382번 해결

---
+++ BAEKJOON/11382.cpp
@@ -0,0 +1,16 @@
+/*
+꼬마 정민
+Bronze 5
+https://www.acmicpc.net/problem/11382
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c;
+}
