d7cd7ed8dcd8a9d474a6ed1cc5ea6bafe3e6817f
KwonSunwon <kso00011147@gmail.com>
2023-10-22 11:10:44 +0900

    15894번 해결

---
+++ BAEKJOON/15894.cpp
@@ -0,0 +1,17 @@
+/*
+수학은 체육과목 입니다.
+Bronze 3
+https://www.acmicpc.net/problem/15894
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long long n;
+    cin >> n;
+
+    cout << n * 4;
+}
