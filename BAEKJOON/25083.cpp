3fabf79bc757aa2f4a67da45801301908f4d163a
KwonSunwon <kso00011147@gmail.com>
2023-10-25 10:12:59 +0900

    25083번 해결

---
+++ BAEKJOON/25083.cpp
@@ -0,0 +1,19 @@
+/*
+새싹
+Bronze 5
+https://www.acmicpc.net/problem/25083
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "         ,r'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |";
+}
