64d32db21b933f9899b84f967435683536762de4
KwonSunwon <kso00011147@gmail.com>
2023-10-30 18:34:52 +0900

    2743번 해결

---
+++ BAEKJOON/2743.cpp
@@ -0,0 +1,17 @@
+/*
+단어 길이 재기
+Bronze 5
+https://www.acmicpc.net/problem/2743
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << str.length();
+}
